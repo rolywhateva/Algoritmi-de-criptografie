e9d1e170174b61fadd395219787869a3fe33e5eb
Veres Roland <roli25veres@gmail.com>
2019-10-22 19:13:53 +0300

    Am adaugat program de citire si scriere in fisier pentru criptosistemul lui Viginere

--- Viginere/Program.cs
+++ Viginere/Program.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace Viginere
 {
     class Program
     {
         static void Main(string[] args)
         {
+            StreamReader reader = new StreamReader("../../textclar.txt");
+            StreamWriter writer = new StreamWriter("../../textcriptat.txt");
+            string key = reader.ReadLine();
+            string line;
+            Vigenere vig = new Vigenere(key);
+            while((line=reader.ReadLine()) !=null)
+            {
+              
+                writer.WriteLine(vig.Criptare(line));
+            }
+            reader.Close();
+            writer.Close();
+
         }
     }
 }
